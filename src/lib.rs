4bf6678b4dd9652980ea9b3cee98671bf4863d33
Drew Crawford <drew@sealedabstract.com>
2022-01-10 18:40:43 -0600

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,17 @@ async fn example() -> Result<(), mac_install::Error> {
     mac_install::install(std::path::Path::new("testdata/test.pkg"),"mypassword".to_owned(),kiruna::Priority::Testing).await
 }
 ```
+
+# Similar libraries
+
+Since Rust is a compiled language, its binaries are self-contained.  Therefore you can write tools to bring up a production or
+development environment in Rust itself, compile them, and shoot them over to new servers via SSH.
+
+You might be interested my expanded universe of sysadmin libraries:
+
+* [rustupr](https://github.com/drewcrawford/rustupr), which installs Rust
+* [github-actions-runner](https://github.com/drewcrawford/github-actions-runner), which installs GitHub's action runner
+* [dmg](https://github.com/drewcrawford/dmg) to mount DMG images
 */
 use std::path::Path;
 use command_rs::Sudo;
